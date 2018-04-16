e7f981594f5b7c162bb5a3c7fa4fe581af22653b
Noah Day <sheltielover432@gmail.com>
2018-04-16 11:38:11 -0400

    Created branch
    This is a branch of the code that I was able to get working, I will upload as I get it to be working more effectively

---
+++ CNM_working/CNM.py
@@ -0,0 +1,14 @@
+import snap
+import importlib
+import graphviz as gv
+from graphviz import Digraph
+
+UGraph = snap.GenRndGnm(snap.PUNGraph, 100, 1000)
+# UGraph=gv.Digraph(open("DataFiles/px4.gv", "r"))
+CmtyV = snap.TCnComV()
+modularity = snap.CommunityCNM(UGraph, CmtyV)
+for Cmty in CmtyV:
+    print ("Community: ")
+    for NI in Cmty:
+        print (NI)
+print ("The modularity of the network is %f" % modularity)
