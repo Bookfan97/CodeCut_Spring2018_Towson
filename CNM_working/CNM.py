d3d408531c40b2eccb46b5cff82050c7b23dc9fd
Noah Day <sheltielover432@gmail.com>
2018-04-27 14:48:16 -0400

    Update
    Created a menu to be able to choose which file to run. Also changed the output to show the community nodes in hex instead of as integers

--- CNM_working/CNM.py
+++ CNM_working/CNM.py
@@ -1,19 +1,28 @@
 import snap
 import importlib
 import graphviz as gv
+from graphviz import Digraph
 
 # UGraph.isNode(x)
 count=0
+graphList=["DataFiles/px4.gv","DataFiles/gnuchess.gv","DataFiles/test.txt","DataFiles/test2.txt"]
 UGraph= snap.PUNGraph.New()
+
+for index in range(0, len(graphList)):
+    # print(index + ": " + graphList[index])
+    print("%(n)s:%(s)s" % {'n': index, 's': graphList[index]})
+x=input("Which graph file would you like to test?")
 # UGraph = snap.GenRndGnm(snap.PUNGraph, 100, 1000)
-with open("DataFiles/test2.txt", "r") as f:
+
+# dot = Digraph(comment=graphList[x])
+with open(graphList[x], "r") as f:
     for line in f:
-        if line.strip()== "}":
+        if line.strip() == "}":
             break
         NodeIDL, NodeIDR= line.split("->")
-        NodeIDR=NodeIDR.split("[") [0]
-        print("NodeIDL (pre-strip): "+NodeIDL)
-        print("NodeIDL (pre-strip): "+NodeIDR)
+        NodeIDR=NodeIDR.split("[")[0]
+        # print("NodeIDL (pre-strip): "+NodeIDL)
+        # print("NodeIDL (pre-strip): "+NodeIDR)
         NodeIDR.strip()
         string1= NodeIDR.split("x",1)[1]
         string2= NodeIDL.strip("s_0x")
@@ -21,21 +30,28 @@ with open("DataFiles/test2.txt", "r") as f:
         b= int(string2, 16)
         if UGraph.IsNode(a) == False:
             UGraph.AddNode(a)
+            # dot.node(string1, str(a))
         if UGraph.IsNode(b) == False:
             UGraph.AddNode(b)
+            # dot.node(string2, str(b))
         if UGraph.IsEdge(a,b)==False:
             UGraph.AddEdge(a, b)
-            print("Edge w/if added")
+            # dot.edge(string1, string2, constraint='false')
+            # print("Edge w/if added")
         count=count+1
-        print("count during for loop: ", count)
-print("Test to see if reached")
+        # print("count during for loop: ", count)
+# print("Test to see if reached")
 CmtyV = snap.TCnComV()
-print("Test to see if reached2")
+# print("Test to see if reached2")
 # modularity = snap.CommunityGirvanNewman(UGraph, CmtyV)
 modularity = snap.CommunityCNM(UGraph, CmtyV)
 for Cmty in CmtyV:
     print ("Community: ")
     for NI in Cmty:
-        print (NI)
+        print (hex(NI))
 print ("The modularity of the network is %f" % modularity)
-print("The count is: ", count)
+# print(dot.source)
+# dot.render(graphList[x], view=True)
+# print("The count is: ", count)
+# dot= Digraph(UGraph)
+# dot.render("DataFiles/px4.gv", view=True)
