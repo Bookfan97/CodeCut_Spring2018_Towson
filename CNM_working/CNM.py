425e4c5e32f98b5693ffc94a2697a44d3a9c6c1b
Noah Day <sheltielover432@gmail.com>
2018-05-04 12:02:28 -0400

    Cleanup
    Cleaned up the commented out code, add a two dimensial array to prepare for the algorithm, as well as writing in comments the general formula for the additional algorithm Ozzie gaves us

--- CNM_working/CNM.py
+++ CNM_working/CNM.py
@@ -3,26 +3,22 @@ import importlib
 import graphviz as gv
 from graphviz import Digraph
 
-# UGraph.isNode(x)
 count=0
 graphList=["DataFiles/px4.gv","DataFiles/gnuchess.gv","DataFiles/px4_subsample_100lines.txt","DataFiles/px4_subsample_762lines.txt"]
 UGraph= snap.PUNGraph.New()
-
+w, h = 100, 100;
+communities=[[0 for x in range(w)] for y in range(h)]
+cmtyindex=0
+nodeindex=0
 for index in range(0, len(graphList)):
-    # print(index + ": " + graphList[index])
     print("%(n)s:%(s)s" % {'n': index, 's': graphList[index]})
 x=input("Which graph file would you like to test?")
-# UGraph = snap.GenRndGnm(snap.PUNGraph, 100, 1000)
-
-# dot = Digraph(comment=graphList[x])
 with open(graphList[x], "r") as f:
     for line in f:
         if line.strip() == "}":
             break
         NodeIDL, NodeIDR= line.split("->")
         NodeIDR=NodeIDR.split("[")[0]
-        # print("NodeIDL (pre-strip): "+NodeIDL)
-        # print("NodeIDL (pre-strip): "+NodeIDR)
         NodeIDR.strip()
         string1= NodeIDR.split("x",1)[1]
         string2= NodeIDL.strip("s_0x")
@@ -30,28 +26,46 @@ with open(graphList[x], "r") as f:
         b= int(string2, 16)
         if UGraph.IsNode(a) == False:
             UGraph.AddNode(a)
-            # dot.node(string1, str(a))
         if UGraph.IsNode(b) == False:
             UGraph.AddNode(b)
-            # dot.node(string2, str(b))
         if UGraph.IsEdge(a,b)==False:
             UGraph.AddEdge(a, b)
-            # dot.edge(string1, string2, constraint='false')
-            # print("Edge w/if added")
         count=count+1
-        # print("count during for loop: ", count)
-# print("Test to see if reached")
 CmtyV = snap.TCnComV()
-# print("Test to see if reached2")
-# modularity = snap.CommunityGirvanNewman(UGraph, CmtyV)
 modularity = snap.CommunityCNM(UGraph, CmtyV)
 for Cmty in CmtyV:
     print ("Community: ")
+    print(cmtyindex)
     for NI in Cmty:
+        communities[cmtyindex][nodeindex]=NI
+        print(nodeindex)
+        nodeindex=nodeindex+1
         print (hex(NI))
+    cmtyindex=cmtyindex+1
+
 print ("The modularity of the network is %f" % modularity)
-# print(dot.source)
-# dot.render(graphList[x], view=True)
-# print("The count is: ", count)
-# dot= Digraph(UGraph)
-# dot.render("DataFiles/px4.gv", view=True)
+print(communities)
+
+# Sort the functions in the cluster by address, lowest first
+#
+# Module_start = <first node address>
+#
+# Temp_module_end = <first node address>
+#
+# Do
+#
+# Next_function1 = <next function in the sorted cluster list>
+#
+# Next_function2 = <next function in the sorted list of all functions>
+#
+# If (Next_function1 == Next_function2)
+#
+#                                 Temp_module_end =  Next_function1
+#
+#                 While (next_function1 == Next_function2)
+#
+#                 Output map entry: module_start – temp_module_end
+#
+#                 #at this point you will have a non-overlapping map file, but it will be out of order
+#
+# Sort map file, and output
