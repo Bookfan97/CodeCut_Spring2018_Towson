1eb7b5c77059fcf0346f20603eba4a1653939166
Noah Day <sheltielover432@gmail.com>
2018-05-05 16:21:10 -0400

    Sort working
    Got the nodes to be grouped together into sorted sublists in python, will start to implement the algorithm given to us soon

--- CNM_working/CNM.py
+++ CNM_working/CNM.py
@@ -6,10 +6,20 @@ from graphviz import Digraph
 count=0
 graphList=["DataFiles/px4.gv","DataFiles/gnuchess.gv","DataFiles/px4_subsample_100lines.txt","DataFiles/px4_subsample_762lines.txt"]
 UGraph= snap.PUNGraph.New()
-w, h = 100, 100;
-communities=[[0 for x in range(w)] for y in range(h)]
+w= 100;
+communities=[[0 for x in range(w)] for y in range(w)]
+clusters=[]
+comm=[]
+# temp=[0 for x in range(w)]
+allnodes=[]
 cmtyindex=0
 nodeindex=0
+c=[]
+community=[0 for x in range(50)]
+t=0
+v=0
+z=0
+string=""
 for index in range(0, len(graphList)):
     print("%(n)s:%(s)s" % {'n': index, 's': graphList[index]})
 x=input("Which graph file would you like to test?")
@@ -26,46 +36,29 @@ with open(graphList[x], "r") as f:
         b= int(string2, 16)
         if UGraph.IsNode(a) == False:
             UGraph.AddNode(a)
+            allnodes.append(a)
         if UGraph.IsNode(b) == False:
             UGraph.AddNode(b)
+            allnodes.append(b)
         if UGraph.IsEdge(a,b)==False:
             UGraph.AddEdge(a, b)
         count=count+1
 CmtyV = snap.TCnComV()
 modularity = snap.CommunityCNM(UGraph, CmtyV)
 for Cmty in CmtyV:
+    string = ""
     print ("Community: ")
-    print(cmtyindex)
     for NI in Cmty:
-        communities[cmtyindex][nodeindex]=NI
-        print(nodeindex)
-        nodeindex=nodeindex+1
         print (hex(NI))
-    cmtyindex=cmtyindex+1
-
+        # print(NI)
+        comm.append(NI)
+    comm.sort()
+    print("Community " + str(v) + ":" + str(comm))
+    clusters.append(comm)
+    comm=[]
+    v = v + 1;
 print ("The modularity of the network is %f" % modularity)
-print(communities)
+print(allnodes)
+print(clusters)
 
-# Sort the functions in the cluster by address, lowest first
-#
-# Module_start = <first node address>
-#
-# Temp_module_end = <first node address>
-#
-# Do
-#
-# Next_function1 = <next function in the sorted cluster list>
-#
-# Next_function2 = <next function in the sorted list of all functions>
-#
-# If (Next_function1 == Next_function2)
-#
-#                                 Temp_module_end =  Next_function1
-#
-#                 While (next_function1 == Next_function2)
-#
-#                 Output map entry: module_start – temp_module_end
-#
-#                 #at this point you will have a non-overlapping map file, but it will be out of order
-#
-# Sort map file, and output
+
