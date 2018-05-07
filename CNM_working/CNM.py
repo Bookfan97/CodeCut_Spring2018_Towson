f70e5a905c6f1b3468e3c6958ae28714f07e75ff
Noah Day <sheltielover432@gmail.com>
2018-05-07 11:47:46 -0400

    Algorithm addition
    Added the algoritihm that was given to us Friday

--- CNM_working/CNM.py
+++ CNM_working/CNM.py
@@ -10,6 +10,7 @@ w= 100;
 communities=[[0 for x in range(w)] for y in range(w)]
 clusters=[]
 comm=[]
+map=[]
 # temp=[0 for x in range(w)]
 allnodes=[]
 cmtyindex=0
@@ -23,6 +24,7 @@ string=""
 for index in range(0, len(graphList)):
     print("%(n)s:%(s)s" % {'n': index, 's': graphList[index]})
 x=input("Which graph file would you like to test?")
+print(str(graphList[x]))
 with open(graphList[x], "r") as f:
     for line in f:
         if line.strip() == "}":
@@ -60,5 +62,39 @@ for Cmty in CmtyV:
 print ("The modularity of the network is %f" % modularity)
 print(allnodes)
 print(clusters)
-
-
+allnodes.sort()
+# Sort map file, and output
+for x in xrange(1,len(clusters)):
+    index=0
+    cluster=clusters[x]
+    print (cluster)
+    moduleStart = clusters[x][index]
+    print("module start: "+str(moduleStart))
+    tempModuleEnd = clusters[x][index]
+    print("tempModuleEnd: "+str(tempModuleEnd))
+    nextFunction1 = clusters[x][index]
+    print("nextFunction1: "+str(nextFunction1))
+    allindex=allnodes.index(moduleStart)
+    print("all index"+str(allindex))
+    nextFunction2 = allnodes[allindex]
+    print("nextFunction2: "+str(nextFunction2))
+    while (nextFunction1 == nextFunction2):
+        index=index+1
+        print ("index: "+str(index))
+        if index>len(cluster)-1:
+            break
+        allindex=allindex+1
+        print("allindex: "+str(allindex))
+        if allindex>len(allnodes)-1:
+            break
+        nextFunction1 = cluster[index]
+        print("nextFunction1: " + str(nextFunction1))
+        nextFunction2 = allnodes[allindex]
+        print("nextFunction2: " + str(nextFunction2))
+        if(nextFunction1 == nextFunction2):
+          tempModuleEnd = nextFunction1
+          print("tempModuleEnd: "+str(tempModuleEnd))
+    mapdata=moduleStart-tempModuleEnd
+    print(mapdata)
+    map.append(mapdata)
+    print(map)
